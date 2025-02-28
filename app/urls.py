a9a3636ce7fb701a745b74a389c94a1fc84cc766
Marcos Paulo <marcosppaivap@gmail.com>
2025-02-28 09:34:12 +0000

    add logout route

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
     path('password-reset/', views.password_reset_view, name='password_reset'),
 ]
